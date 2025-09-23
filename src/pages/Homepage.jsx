ef1da196f00d69df644162db272e5cf390ac04f8
mohaneswar123 <pvmohaneswar@gmail.com>
2025-09-23 08:47:46 +0530

    commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -147,14 +147,14 @@ const Homepage = () => {
         singleItem={selectedBookForPurchase}
       />
 
-      {/* Footer */}
+      {/* Footer
       <footer className="bg-white/50 border-t border-gray-100/50 mt-20">
         <div className="max-w-7xl mx-auto px-6 lg:px-8 py-12">
           <div className="text-center text-gray-400">
             <p className="font-light font-['Lato',_sans-serif]">&copy; 2024 InkɅura. All rights reserved.</p>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 };
